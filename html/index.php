e677371e0cbb5d917ae4b4cdbaa1ffbfadbd29f9
wn1980 <waipotn@hotmail.com>
2023-08-29 06:11:02 +0700

    update

---
+++ html/index.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'db';
+$user = 'root';
+$pass = '123456';
+
+$conn = new mysqli($host, $user, $pass);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL server successfully!";
+}
+?>
